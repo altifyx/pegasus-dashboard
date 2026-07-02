15136e7c2093b99a4f8ab3bc3bb31c8c4c9f709e
semi-constructor <toowake@proton.me>
2026-07-02 14:08:36 +0000

    fix(auth): explicitly pass NextRequest to NextAuth handlers to fix headers error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,9 @@
 import { handlers } from "@/auth"
-export const { GET, POST } = handlers
+import { NextRequest } from "next/server"
+
+export const GET = (req: NextRequest) => {
+  return handlers.GET(req)
+}
+export const POST = (req: NextRequest) => {
+  return handlers.POST(req)
+}
